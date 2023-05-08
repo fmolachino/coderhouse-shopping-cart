b66627b8d37f1f574412503a518d1a63bae9f3f4
fmolachino <fmolachino@gmail.com>
2023-05-08 19:56:53 -0300

    added items, need a litle colouring

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 // product data. TODO: be in a json and then parse it.
 const products = [
-    { id: 1, name: 'Barret M82', price: 90000.99, image: "../images/guns/barret-m82.jpg"},
-    { id: 2, name: 'Product 2', price: 19.99, image: 'https://picsum.photos/200' },
-    { id: 3, name: 'Product 3', price: 7.99, image: 'https://picsum.photos/200' },
-    { id: 4, name: 'Product 4', price: 12.99, image: 'https://picsum.photos/200' },
+    { id: 1, name: 'Barret M82', price: 90000.99, image: "./images/guns/barret-m82.jpg"},
+    { id: 2, name: 'Bruni 45', price: 990.70, image: "./images/guns/bruni-45.jpg" },
+    { id: 3, name: 'High Point 45', price: 1200.99, image: "./images/guns/high-point-45.jpg" },
+    { id: 4, name: 'Kimar 8', price: 800.99, image: "./images/guns/kimar-8.jpg" },
+    { id: 5, name: 'Rifle M4', price: 7500.99, image: "./images/guns/m4.jpg" },
+    { id: 6, name: 'Revolver 22', price: 500.99, image: "./images/guns/mini-gun-22.jpg" },
+    { id: 7, name: 'MP 22', price: 950.99, image: "./images/guns/mp-22.jpg" },
+    { id: 8, name: 'Ammo Box 1000 Rounds', price: 600.99, image: "./images/ammo/ammo-box-9.jpg" },
+    { id: 9, name: 'Luger 9 - 100 Rounds', price: 120.99, image: "./images/ammo/ammo-lurger-100-9.jpg" },
+    { id: 10, name: 'Winchester 9 - 100 Rounds', price: 150.99, image: "./images/ammo/ammo-winchester-100-9.jpg" },
+    { id: 11, name: 'Barret M82', price: 90000.99, image: "./images/guns/barret-m82.jpg"},
+    { id: 12, name: 'Bruni 45', price: 990.70, image: "./images/guns/bruni-45.jpg" },
+    { id: 13, name: 'High Point 45', price: 1200.99, image: "./images/guns/high-point-45.jpg" },
+    { id: 14, name: 'Kimar 8', price: 800.99, image: "./images/guns/kimar-8.jpg" },
+    { id: 15, name: 'Rifle M4', price: 7500.99, image: "./images/guns/m4.jpg" },
+    { id: 16, name: 'Revolver 22', price: 500.99, image: "./images/guns/mini-gun-22.jpg" },
 ];
 
 // cart init
