f007139e07063a46b20a43ceefa6ee90eeab8649
fmolachino <fmolachino@gmail.com>
2023-05-17 08:02:20 -0300

    now fetching products from a json, and implemented local storage usage

--- js/main.js
+++ js/main.js
@@ -1,43 +1,48 @@
-// product data. TODO: be in a json and then parse it.
-const products = [
-    { id: 1, name: 'Barret M82', price: 90000.99, image: "./images/guns/barret-m82.jpg"},
-    { id: 2, name: 'Bruni 45', price: 990.70, image: "./images/guns/bruni-45.jpg" },
-    { id: 3, name: 'High Point 45', price: 1200.99, image: "./images/guns/high-point-45.jpg" },
-    { id: 4, name: 'Kimar 8', price: 800.99, image: "./images/guns/kimar-8.jpg" },
-    { id: 5, name: 'Rifle M4', price: 7500.99, image: "./images/guns/m4.jpg" },
-    { id: 6, name: 'Revolver 22', price: 500.99, image: "./images/guns/mini-gun-22.jpg" },
-    { id: 7, name: 'MP 22', price: 950.99, image: "./images/guns/mp-22.jpg" },
-    { id: 8, name: 'Ammo Box 1000 Rounds', price: 600.99, image: "./images/ammo/ammo-box-9.jpg" },
-    { id: 9, name: 'Luger 9 - 100 Rounds', price: 120.99, image: "./images/ammo/ammo-lurger-100-9.jpg" },
-    { id: 10, name: 'Winchester 9 - 100 Rounds', price: 150.99, image: "./images/ammo/ammo-winchester-100-9.jpg" },
-    { id: 11, name: 'Barret M82', price: 90000.99, image: "./images/guns/barret-m82.jpg"},
-    { id: 12, name: 'Bruni 45', price: 990.70, image: "./images/guns/bruni-45.jpg" },
-    { id: 13, name: 'High Point 45', price: 1200.99, image: "./images/guns/high-point-45.jpg" },
-    { id: 14, name: 'Kimar 8', price: 800.99, image: "./images/guns/kimar-8.jpg" },
-    { id: 15, name: 'Rifle M4', price: 7500.99, image: "./images/guns/m4.jpg" },
-    { id: 16, name: 'Revolver 22', price: 500.99, image: "./images/guns/mini-gun-22.jpg" },
-];
+const url = 'js/products.json';
+let productData;
+let cart = [];
 
-// cart init
-const cart = [];
 
-// fill product-list ul as div elements
-const productList = document.getElementById('product-list');
-products.forEach(product => {
-    const li = document.createElement('div');
-    li.innerHTML = `
-        <img src=${product.image} />
-        <h3>${product.name} - $${product.price}</h3>
-        <input type="number" id="product-quantity-${product.id}" min="1" value="1">
-        <br><br>
-        <button onclick="addToCart(${product.id})">Add to Cart</button>
-    `;
-    productList.appendChild(li);
-});
+// get localStorage data and uptdates cart whenever the page is refreshed or enter into
+window.addEventListener('load', () => {
+    const cartData = localStorage.getItem('cart');
+    if (cartData) {
+      cart = JSON.parse(cartData);
+    }
+    updateCart();
+  });
+
+// fetch the JSON data from the URL
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    
+    //save the json data into productData variable
+    productData = data;
+
+    // fill product-list ul as div elements
+    const productList = document.getElementById('product-list');
+    productData.forEach(product => {
+        const li = document.createElement('div');
+        li.innerHTML = `
+            <img src=${product.image} />
+            <h3>${product.name} - $${product.price}</h3>
+            <input type="number" id="product-quantity-${product.id}" min="1" value="1">
+            <br><br>
+            <button onclick="addToCart(${product.id})">Add to Cart</button>
+        `;
+        productList.appendChild(li);
+    });
+  })
+  .catch(error => {
+    // Handle any errors that occurred during the fetch
+    console.error('Error:', error);
+  });
+
 
 // add the item to the cart
 function addToCart(productId) {
-    const product = products.find(p => p.id === productId);
+    const product = productData.find(p => p.id === productId);
     const quantity = parseInt(getQuantity(productId).value);
 
     if (quantity > 0) {
@@ -48,6 +53,8 @@ function addToCart(productId) {
             cart.push({ product, quantity });
         }
         
+        localStorage.setItem('cart', JSON.stringify(cart));
+
         updateCart();
     }
 }
@@ -58,6 +65,9 @@ function removeFromCart(productId) {
     const index = cart.findIndex(item => item.product.id === productId);
     if (index !== -1) {
         cart.splice(index, 1);
+
+        localStorage.setItem('cart', JSON.stringify(cart));
+
         updateCart();
     }
 }
@@ -89,6 +99,7 @@ checkoutBtn.addEventListener('click', () => {
     
     alert('Checkout successful!');
     cart.length = 0; // clear cart, as no logic is yet implemented on the checkout
+    localStorage.removeItem('cart');
     updateCart();
 });
 
