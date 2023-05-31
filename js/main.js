23a73659c6dfac8a38d4931a3036e3245401efa3
fmolachino <fmolachino@gmail.com>
2023-05-31 19:10:44 -0300

    asyn await implemented. sweetAlert usage, and added controller to logics

--- js/main.js
+++ js/main.js
@@ -12,36 +12,32 @@ window.addEventListener('load', () => {
     updateCart();
   });
 
-// fetch the JSON data from the URL
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    
-    //save the json data into productData variable
-    productData = data;
-
-    // fill product-list ul as div elements
-    const productList = document.getElementById('product-list');
-    productData.forEach(product => {
+// get the JSON data from the URL and parse it
+(async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      productData = data;
+  
+      const productList = document.getElementById('product-list');
+      productData.forEach(product => {
         const li = document.createElement('div');
         li.innerHTML = `
-            <img src=${product.image} />
-            <h3>${product.name}</h3>
-            <h3>$${product.price}<h3>
-            <input type="number" id="product-quantity-${product.id}" min="1" value="1">
-            <br><br>
-            <button onclick="addToCart(${product.id})">Add to Cart</button>
+          <img src=${product.image} />
+          <h3>${product.name}</h3>
+          <h3>$${product.price}<h3>
+          <input type="number" id="product-quantity-${product.id}" min="1" value="1">
+          <br><br>
+          <button onclick="addToCart(${product.id})">Add to Cart</button>
         `;
         productList.appendChild(li);
-    });
-  })
-  .catch(error => {
-    // Handle any errors that occurred during the fetch
-    console.error('Error:', error);
-  });
+      });
+    } catch (error) {
+      console.error('Error while loading products', error);
+    }
+  })();
 
-
-// add the item to the cart
+// add item to the cart
 function addToCart(productId) {
     const product = productData.find(p => p.id === productId);
     const quantity = parseInt(getQuantity(productId).value);
@@ -56,6 +52,17 @@ function addToCart(productId) {
         
         localStorage.setItem('cart', JSON.stringify(cart));
 
+        Swal.fire({
+            icon: 'success',
+            title: 'Added to Cart',
+            text: `${product.name} - ${quantity} item(s) added to cart.`,
+            timer: 2000,
+            timerProgressBar: true,
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false
+          });
+
         updateCart();
     }
 }
@@ -65,15 +72,28 @@ function addToCart(productId) {
 function removeFromCart(productId) {
     const index = cart.findIndex(item => item.product.id === productId);
     if (index !== -1) {
+        const removedProduct = cart[index].product;
         cart.splice(index, 1);
 
         localStorage.setItem('cart', JSON.stringify(cart));
+
+        //sweetAlert message
+        Swal.fire({
+            icon: 'success',
+            title: 'Removed from Cart',
+            text: `${removedProduct.name} removed from cart.`,
+            timer: 2000,
+            timerProgressBar: true,
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false
+          });
 
         updateCart();
     }
 }
 
-// update cart ui
+// update cart UI
 function updateCart() {
     const cartItems = document.getElementById('cart-items');
     const totalPrice = document.getElementById('total-price');
@@ -94,14 +114,39 @@ function updateCart() {
     totalPrice.innerText = total.toFixed(2);
 }
 
-// checkout>>>>>>>>>
+// checkout logic and its controler
 const checkoutBtn = document.getElementById('checkout-btn');
 checkoutBtn.addEventListener('click', () => {
-    
-    alert('Checkout successful!');
-    cart.length = 0; // clear cart, as no logic is yet implemented on the checkout
-    localStorage.removeItem('cart');
-    updateCart();
+    if(cart.length==0){
+        //sweetAlert message
+        Swal.fire({
+            icon: 'error',
+            title: 'Empty cart',
+            text: `Cannot proceed, cart is empty.`,
+            timer: 10000,
+            timerProgressBar: true,
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: true
+          });
+    }
+
+    else{
+        //sweetAlert message
+        Swal.fire({
+            icon: 'success',
+            title: 'Purchase successfully.',
+            text: `Congratulations for your purchase. Total price: $${document.getElementById('total-price').innerText}`,
+            timer: 10000,
+            timerProgressBar: true,
+            toast: true,
+            position: 'center',
+            showConfirmButton: true
+          });
+        cart.length = 0; // clear cart, as no logic is yet implemented on the checkout
+        localStorage.removeItem('cart');
+        updateCart();
+    }
 });
 
 
